382c0f320aaae8ba01e2534a5667e33efc535de8
Yukti Pandey <yuktipandey661@gmail.com>
2025-09-14 16:06:43 +0530

    keep only README.md

--- ElementsPractice/Level1/Program3.java
+++
@@ -1,7 +0,0 @@
-public class Program3 {
-    public static void main(String[] args) {
-        double km = 10.8;
-        double miles = km / 1.6;
-        System.out.println("The distance " + km + " km in miles is " + miles);
-    }
-}
